378f51e7d6d6afd2214487a023e65f07ea411ba6
magicbaer <magicbaer@baerchain.net>
2019-08-24 19:14:02 +0800

    log

--- libbrcdchain/BlockChainSync.cpp
+++ libbrcdchain/BlockChainSync.cpp
@@ -223,7 +223,7 @@ void BlockChainSync::syncPeer(NodeID const& _peerID, bool _force)
     if (m_host.peer(_peerID).isConversing())
     {
         LOG(m_loggerDetail) << "Can't sync with this peer - outstanding asks."
-                            << "ask state " << ::toString(m_host.peer(_peerID).asking());
+                            << "ask state " << ::toString(m_host.peer(_peerID).asking())
                             << "ask state " << ::toString(m_host.peer(_peerID).latestHash());
         return;
     }
